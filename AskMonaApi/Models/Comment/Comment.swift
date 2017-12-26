a5e5accbfcace43abef3413747c8cea799b42e2b
kz56cd <kz56cd@gmail.com>
2017-12-26 10:41:50 +0900

    Update comment entity

--- AskMonaApi/Models/Comment/Comment.swift
+++ AskMonaApi/Models/Comment/Comment.swift
@@ -9,10 +9,31 @@
 import Foundation
 
 public struct Comment: Codable {
+    let commentId: Int
+    let state: Int
     let created: Int
-    // WIP
+    let userId: Int
+    let userName: String
+    let userDan: String
+    let userCommentTimes: String
+    let receivedMonas: String
+    let receivedMonasLevel: Int
+    let receivedMonasUsersCount: Int
+    let host: String?
+    let content: String
     
     enum CodingKeys: String, CodingKey {
+        case commentId = "r_id"
+        case state
         case created
+        case userId = "u_id"
+        case userName = "u_name"
+        case userDan = "u_dan"
+        case userCommentTimes = "u_times"
+        case receivedMonas = "receive"
+        case receivedMonasLevel = "res_lv"
+        case receivedMonasUsersCount = "rec_count"
+        case host
+        case content = "response"
     }
 }
