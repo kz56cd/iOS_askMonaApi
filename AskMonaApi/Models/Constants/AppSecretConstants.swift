d6baec0808532c8b93aaf8036c19c31aa2e95faf
kz56cd <kz56cd@gmail.com>
2017-12-28 20:02:46 +0900

    Add AppSecretProvider

---
+++ AskMonaApi/Models/Constants/AppSecretConstants.swift
@@ -0,0 +1,13 @@
+//
+//  AppSecretConstants.swift
+//  AskMonaApi
+//
+//  Created by msano on 2017/12/28.
+//  Copyright © 2017年 kz56cd. All rights reserved.
+//
+
+import Foundation
+
+public struct AppSecretConstants: Codable {
+    // stub
+}
